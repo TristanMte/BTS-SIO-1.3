81b5bfb3633a355bd5643ef6a0a69459332b1643
TristanMte <115156165+TristanMte@users.noreply.github.com>
2022-12-01 16:00:30 +0100

    Add files via upload

--- Site JS/Style JS.js
+++ Site JS/Style JS.js
@@ -146,3 +146,78 @@ function releve(){
     else if (t>20)
         alert("Erreur")
 }
+
+
+function affichagetab1(){
+    document.write('<table border=2px width=30%');
+    for(let i = 0; i <= 5; i++){
+        document.write('<tr> <td>1</td> <td>2</td> <td>3</td> </tr>');
+    }
+    document.write('</table>');
+}
+
+function affichagetab2(){
+    document.write('<table border=2px width=30%');
+    var ligne=prompt("Combien de lignes dans le tableau?")
+    for(let i = 1; i <= ligne; i++){
+        document.write("<tr> <td>"+i+"</td> <td>2</td> <td>3</td> </tr>");
+    }
+    document.write('</table>');
+}
+
+function seconnecter1(){
+    var log=prompt("Quel est votre identifiant?");
+    var pwd=prompt("Entrez le mot de passe");
+
+    if (log=='admin' & pwd=='password')
+        document.write("Bonjour, vous etes en acces administrateur")
+    else
+        alert("Mauvais identifiant/mot de passe !")
+}
+
+function seconnecter2(){
+    var i=0;
+    do{
+        var log=prompt("Quel est votre identifiant?");
+        var pwd=prompt("Entrez le mot de passe");
+        if (log=='admin' & pwd=='password'){
+        document.write("Bonjour, vous etes en acces administrateur, " + log);
+        break;
+        }
+        else
+            alert("Mauvais identifiant/mot de passe !");
+            i+=1;
+    }
+    while (i<3);
+    if(i==3)
+        alert("Trop de tentative, delai depasse!")
+}
+
+function cdc(){
+    var chaine=prompt("Donner un mot");
+    document.write(chaine.toUpperCase()+"<br><br>");
+    document.write(chaine.toLowerCase()+"<br><br>");
+    document.write(" La chaine contient " + chaine.length+ " caracteres " + "<br><br>");
+    document.write(" La premiere lettre de la chaine est "+ chaine.substring(0,1));
+}
+
+function switch1(){
+    var color=prompt("Entrez une couleur");
+    switch(color)
+    {
+        case "rouge": document.body.style.background="red";
+        break;
+        case "orange": document.body.style.background="orange";
+        break;
+        case "jaune": document.body.style.background="yellow";
+        break;
+        case "vert": document.body.style.background="green";
+        break;
+        case "bleu": document.body.style.background="blue";
+        break;
+        case "violet": document.body.style.background="violet";
+        break;
+
+        default:alert("Couleur non repertoriee")
+    }
+}
